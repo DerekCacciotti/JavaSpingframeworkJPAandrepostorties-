182d6ad570e5cf6e933ffc17049c9801befa1fd4
Derek Cacciotti <derekcacciotti@dereks-mbp-2.lan>
2019-01-18 15:44:54 -0500

    added spring mvc added template engine made a web application to display a list of names and authors from the h2 db

--- src/main/java/com/derekcacciotti/spring5webapp/controllers/AuthorController.java
+++ src/main/java/com/derekcacciotti/spring5webapp/controllers/AuthorController.java
@@ -1,4 +1,28 @@
 package com.derekcacciotti.spring5webapp.controllers;
 
+import com.derekcacciotti.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AuthorController {
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+
+
+
+    }
+
+
+    @RequestMapping("/authors")
+
+    public String GetAuthors(Model model)
+    {
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors";
+    }
 }
